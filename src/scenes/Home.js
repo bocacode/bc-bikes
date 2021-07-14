ecfb17c3077ffb66eccad36c84d71cb937e8c03b
Todd Albert <todd@bocacode.com>
2021-07-14 19:53:01 -0400

    added firebase auth

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,9 +1,18 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { AuthContext } from '../App'
 
 function Home(){
     const { isLoggedIn, user } = useContext(AuthContext)
-    return <h1>Hello {isLoggedIn ? user.name : 'Guest'}</h1>
+    useEffect(() => {
+        console.log({ user })
+    }, [user])
+    return (
+        <>
+            <h1>Hello {isLoggedIn ? user.displayName || 'friend' : 'Guest'}</h1>
+            {isLoggedIn &&
+                <img src={user.photoURL} alt={user.displayName} />}
+        </>
+    )
 }
 
 export default Home
--- src/scenes/Login.js
+++ src/scenes/Login.js
@@ -1,28 +1,72 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import firebase from "firebase/app"
+import "firebase/auth"
 import { AuthContext } from '../App'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAnq-vbEw35Ktfs-NKjYqNnqF91TAD-yQc",
+    authDomain: "bc-bikes-ta.firebaseapp.com",
+    projectId: "bc-bikes-ta",
+    storageBucket: "bc-bikes-ta.appspot.com",
+    messagingSenderId: "807463870071",
+    appId: "1:807463870071:web:cd9de58309451f95267072"
+}
+
+firebase.initializeApp(firebaseConfig)
 
 function Login() {
     const { isLoggedIn, setIsLoggedIn, setUser } = useContext(AuthContext)
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
     const clickHandler = () => {
-        if(isLoggedIn) {
+        if (isLoggedIn) {
             setUser(null)
             setIsLoggedIn(false)
         } else {
-            setUser({
-                name: 'Sara',
-                role: 'Admin'
-            })
-            setIsLoggedIn(true)
+            firebase.auth().signInWithEmailAndPassword(email, password)
+                .then(res => {
+                    setUser(res.user)
+                    setIsLoggedIn(true)
+                })
+                .catch(err => alert(err.message))
         }
     }
-  return (
-    <>
-        <h1>{isLoggedIn ? 'Log out' : 'Login'}</h1>
-        <button onClick={() => clickHandler()}>
-            {isLoggedIn ? 'Log out' : 'Login'}
-        </button>
-    </>
-  )
+    const formValHandler = (e) => {
+        e.preventDefault()
+        if(e.target.name === 'email') {
+            setEmail(e.target.value)
+        } else {
+            setPassword(e.target.value)
+        }
+    }
+    const loginWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        firebase.auth().signInWithPopup(provider)
+            .then(res => {
+                setUser(res.user)
+                setIsLoggedIn(true)
+            })
+            .catch(err => alert(err.message))
+    }
+    return (
+        <>
+            <h1>{isLoggedIn ? 'Log out' : 'Login'}</h1>
+            <form>
+                <label>Email: 
+                    <input name="email" type="email" value={email} onChange={formValHandler} />
+                </label>
+                <label>Password: 
+                    <input name="password" type="password" value={password} onChange={formValHandler} />
+                </label>
+            </form>
+            <button onClick={() => clickHandler()}>
+                {isLoggedIn ? 'Log out' : 'Login'}
+            </button>
+            <br/>
+            {!isLoggedIn && 
+                <button onClick={() => loginWithGoogle()}>Login with Google</button>}
+        </>
+    )
 }
 
 export default Login
